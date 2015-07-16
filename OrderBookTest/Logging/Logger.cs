5876bf5a2f32f07e4ecdcbd1a419b184d467dbe0
Manoj Sethi <neoms21@gmail.com>
2015-07-16 23:08:45 +0100

    Logger clean up!!

--- OrderBookTest/Logging/Logger.cs
+++ OrderBookTest/Logging/Logger.cs
@@ -3,21 +3,14 @@ using CMCMarkets.Prophet.OrderBookTest.api;
 
 namespace CMCMarkets.Prophet.OrderBookTest.Logging
 {
-
     public class Logger : ILog
     {
         private const string LogFilePath = "Log.log";
 
-        public Logger()
-        {
-            //if (!File.Exists(LogFilePath))
-            //    File.Create(LogFilePath);
-
-        }
         public void log(LogLevel level, string msg)
         {
-            using (FileStream fs = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
-            using (StreamWriter sw = new StreamWriter(fs))
+            using (var fs = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
+            using (var sw = new StreamWriter(fs))
             {
                 sw.WriteLine(msg);
             }
