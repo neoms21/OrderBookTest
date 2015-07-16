a383306d653d47ef05b26eb2794027a188ea4062
Manoj Sethi <neoms21@gmail.com>
2015-07-16 23:07:24 +0100

    namespaces moved outside

--- OrderBookTest/Program.cs
+++ OrderBookTest/Program.cs
@@ -1,7 +1,7 @@
-﻿namespace CMCMarkets.Prophet.OrderBookTest
+﻿using CMCMarkets.Prophet.OrderBookTest.consumer;
+using CMCMarkets.Prophet.OrderBookTest.api;
+namespace CMCMarkets.Prophet.OrderBookTest
 {
-    using CMCMarkets.Prophet.OrderBookTest.consumer;
-    using CMCMarkets.Prophet.OrderBookTest.api;
 
     class Program
     {
@@ -9,7 +9,7 @@
         {
             var environment = new AppEnvironmentImpl(LogLevel.INFO);
             var consumer = new OrderConsumer();
-            
+
             environment.ProcessingStartEvent += consumer.StartProcessing;
             environment.ProcessingFinishEvent += consumer.FinishProcessing;
             environment.OrderActionEvent += consumer.HandleOrderAction;
